b35fe8ab50a6042c438d5611a05910cfe206bce8
Will <doggy.huang@gmail.com>
2022-02-13 00:26:26 +0800

    將 JwtHelpers 類別註冊進 .NET 的 DI 容器中

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddSingleton<JwtHelpers>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
